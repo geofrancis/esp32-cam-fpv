3e03f19ce5911a439d7724f423b8a3049611d028
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2023-12-12 23:37:43 +0100

    updated drivers

--- rpi_drivers/5.10.103-v7+/install.sh
+++ rpi_drivers/5.10.103-v7+/install.sh
@@ -1,3 +1,18 @@
-sudo cp brcmfmac.ko /lib/modules/5.10.103-v7+/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko
-sudo cp brcmfmac43436-sdio.bin /usr/lib/firmware/brcm/
+sudo su
+apt-get update && apt-get full-upgrade
+sudo apt install raspberrypi-kernel-headers git libgmp3-dev gawk qpdf bison flex make autoconf libtool texinfo
+git clone https://github.com/seemoo-lab/nexmon.git
+cd nexmon
+source setup_env.sh
+make
+cd patches/bcm43436b0/9_88_4_65/nexmon/
+make
+make backup-firmware
+make install-firmware
+cd /home/pi/
+cd utilities/nexutil/
+make && make install
+iw dev wlan0 set power_save off
+cp /home/pi/nexmon/patches/bcm43430a1/7_45_41_46/nexmon/brcmfmac_kernel49/brcmfmac.ko /lib/modules/5.10.103-v7+/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko
 depmod -a
+reboot
