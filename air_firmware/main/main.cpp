973a31aa97a577cca88c0e14cd841c66532e347c
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2023-12-12 02:31:09 +0100

    fixed camera frequency

--- air_firmware/main/main.cpp
+++ air_firmware/main/main.cpp
@@ -695,7 +695,7 @@ static void init_camera()
     config.pin_sccb_scl = SIOC_GPIO_NUM;
     config.pin_pwdn = PWDN_GPIO_NUM;
     config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
+    config.xclk_freq_hz = 12000000;
     config.pixel_format = PIXFORMAT_JPEG;
     config.frame_size = FRAMESIZE_SVGA;
     config.jpeg_quality = 4;
