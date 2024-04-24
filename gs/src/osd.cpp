7f36e120055a6b27b1dea6cf4eb7117e66736f21
RomanLut <11955117+RomanLut@users.noreply.github.com>
2024-04-24 01:02:22 +0100

    fixed osd rendering

--- gs/src/osd.cpp
+++ gs/src/osd.cpp
@@ -35,14 +35,26 @@ void OSD::draw()
     for ( int row = 0; row < OSD_ROWS; row++ )
     {
         int x = mx;
+        int mask = 1;
+        int col8 = 0;
         for ( int col = 0; col < OSD_COLS; col++ )
         {
             uint16_t c = this->buffer.screenLow[row][col];
+            if ( (this->buffer.screenHigh[row][col8] & mask) !=0 )
+            {
+                c += 0x100;
+            }
             if ( c != 0 )
             {
                 this->font->drawChar(c, x, y, ixs, iys);
             }
             x += ixs;
+            mask <<= 1;
+            if ( mask == 0x100 )
+            {
+                mask = 1;
+                col8++;
+            }
         }
         y += iys;
     }
@@ -53,6 +65,5 @@ void OSD::draw()
 void OSD::update(void* pScreen)
 {
     memcpy(&this->buffer, pScreen, OSD_BUFFER_SIZE);
-    this->buffer.screenLow[10][10]++;
 }
 
